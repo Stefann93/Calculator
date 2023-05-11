2b989661d7eba9df97da88800fb0d540d78096ab
Stefann93 <ilic.stefann4@gmail.com>
2023-05-11 12:27:42 +0200

    Started working on Script branch Things left to do: Fix the "C" button, right now it just removes the last character on the display, but keeps number variable as it was. Add function to the "." button. Make it so that when I press an operand it check if I have the result and if I do, it makes that result the first number. Make dividing by 0 impossible.

--- script.js
+++ script.js
@@ -1,27 +1,117 @@
 const calcStatus = document.querySelector(".status");
 const powerButton = document.querySelector(".powerBtn");
-let num1,num2;
-let isCalcOn = false;
+const numbers = document.querySelectorAll(".number");
+const digits = document.querySelector(".digits");
+const operands = document.querySelectorAll(".operand");
+const equals = document.querySelector("#equals");
+const calcResult = document.querySelector(".result");
+const ac = document.querySelector("#ac");
+const c = document.querySelector("#c");
+const sign = document.querySelector("#sign")
 
+let num1 = '', num2 = '', plusMinus = "+", isCalcOn = false;
+let result, operand;
 powerButton.addEventListener("click", function () {
-    if (isCalcOn == false && num1 == null) {
+    if (isCalcOn == false) {
         calcStatus.style.backgroundColor = 'green';
         isCalcOn = true;
     } else {
         calcStatus.style.backgroundColor = 'red';
+        AC();
         isCalcOn = false;
     }
 });
+numbers.forEach(number =>
+    number.addEventListener('click', function () {
+        if (isCalcOn == true) {
+            if (operand == null) {
+                num1 += number.textContent;
+                digits.textContent = num1;
+            }
+            else if (operand != null) {
+                num2 += number.textContent;
+                digits.textContent = `${num1} ${operand} ${num2}`;
+            }
+        } else window.alert("Turn on the calculator first");
+    }
+    ));
+operands.forEach(element =>
+    element.addEventListener("click", function () {
+        if (isCalcOn == true && num1 != '') {
+            operand = element.textContent;
+            digits.textContent = `${num1} ${operand}`;
+        }
+    }));
 
-function Add(num1, num2) {
-    return num1 + num2;
+equals.addEventListener("click", function () {
+    if (operand != null && num1 != '' && num2 != '')
+        switch (operand) {
+            case "+": Add(num1, num2); calcResult.textContent = result; break;
+            case "-": Subtract(num1, num2); calcResult.textContent = result; break;
+            case "X": Multiply(num1, num2); calcResult.textContent = result; break;
+            case "/": Divide(num1, num2); calcResult.textContent = result; break;
+            default: window.alert("Error");
+        }
+});
+ac.addEventListener("click", AC);
+c.addEventListener("click", C);
+
+sign.addEventListener("click", function () {
+    if (operand == null && num1 != '') {
+        if (plusMinus == "+") {
+            num1 = `-${num1}`;
+            digits.textContent = num1;
+            plusMinus = "-"
+        } else if (plusMinus == "-") {
+            num1 = digits.textContent.substring(1);
+            digits.textContent = num1;
+            plusMinus = "+";
+        }
+    }
+});
+
+function Add(number1, number2) {
+    result = Math.round((parseFloat(number1) + parseFloat(number2)) * 100) / 100;
+    num1 = '';
+    num2 = '';
+    operand = null;
+    plusMinus = "+";
+    return result;
 }
-function Subtract(num1, num2) {
-    return num1 - num2;
+function Subtract(number1, number2) {
+    result = Math.round((parseFloat(number1) - parseFloat(number2)) * 100) / 100;
+    num1 = '';
+    num2 = '';
+    operand = null;
+    plusMinus = "+";
+    return result;
 }
-function Multiply(num1, num2) {
-    return num1 * num2;
+function Multiply(number1, number2) {
+    result = Math.round((parseFloat(number1) * parseFloat(number2)) * 100) / 100;
+    num1 = '';
+    num2 = '';
+    operand = null;
+    plusMinus = "+";
+    return result;
 }
-function Divide(num1, num2) {
-    return num1 / num2;
+function Divide(number1, number2) {
+    result = Math.round((parseFloat(number1) / parseFloat(number2)) * 100) / 100;
+    num1 = '';
+    num2 = '';
+    operand = null;
+    plusMinus = "+";
+    return result;
+}
+function AC() {
+    num1 = '';
+    num2 = '';
+    result = null;
+    operand = null;
+    digits.textContent = '';
+    calcResult.textContent = '';
+    plusMinus = "+";
+}
+function C() {
+    let str = digits.textContent.slice(0, -1);
+    digits.textContent = str;
 }
